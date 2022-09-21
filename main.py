71c47a53798531115a02531ce4658790351fafd6
iman aflaki <113721468+iman-aflaki@users.noreply.github.com>
2022-09-21 12:11:55 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def has_consecutive_letter(string_):
-    return any(c1 == c2 for c1, c2 in zip(string_,string_[1:]))
+    return any(c1 == c2 for c1, c2 in zip(string_, string_[1:]))
 str1 = '''bobby goes out with daddy and mommy and talked about his hobbies'''
 
 str2 = str1.split()
